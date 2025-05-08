64814dea234e132324ae4d4bed81314c4877964c
karim <kimo2004karim@gmail.com>
2025-05-08 17:48:39 +0300

    add user model

---
+++ lib/model/user_model.dart
@@ -0,0 +1,14 @@
+class User_model {
+  final String Name;
+  final String Email;
+  final String password;
+  final String image;
+
+  User_model({
+    required this.Name,
+    required this.Email,
+    required this.password,
+    required this.image,
+  });
+
+}
