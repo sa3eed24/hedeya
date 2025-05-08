130dac95c87ed051b6dc1e56e19d4f40974c84cd
karim <kimo2004karim@gmail.com>
2025-05-08 19:04:43 +0300

    added all models

---
+++ lib/model/event_model.dart
@@ -0,0 +1,14 @@
+class event_model{
+  final String Name;
+  final int id;
+  final String category;
+  final String status;
+
+  event_model({
+    required this.Name,
+    required this.id,
+    required this.category,
+    required this.status
+  });
+
+}
---
+++ lib/model/gift_model.dart
@@ -0,0 +1,16 @@
+class gift_model {
+  final String name;
+  final String description;
+  final double price;
+  final String image;
+  final String status;
+
+  gift_model({
+    required this.name,
+    required this.description,
+    required this.price,
+    required this.image,
+    required this.status,
+  });
+
+}
